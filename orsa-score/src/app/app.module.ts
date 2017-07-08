6efc6fca1ce29f7c6e5094302bde3464c200c23d
Lojdquist <axel.lojdquist@hotmail.com>
2017-07-08 15:54:06 +0200

    admin v1

--- orsa-score/src/app/app.module.ts
+++ orsa-score/src/app/app.module.ts
@@ -4,12 +4,14 @@ import {AppRoutingModule} from './app-routing.module'
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { AdminComponent } from './admin/admin.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    DashboardComponent
+    DashboardComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
