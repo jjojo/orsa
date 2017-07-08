e87a2499bdd5ddf1027dff2308dc61ef4bc7e9de
jonasj <jonasj9@kth.se>
2017-07-08 18:40:32 +0200

    firebase integration initiated

--- orsa-score/src/app/app.module.ts
+++ orsa-score/src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { environment } from '../environments/environment';
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 
--- orsa-score/src/environments/environment.ts
+++ orsa-score/src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDw5qPzxJ5-2QxM9np8Q18AAmW8Qgq-7UE',
+    authDomain: 'orsa-app.firebaseapp.com',
+    databaseURL: 'https://orsa-app.firebaseio.com',
+    projectId: 'orsa-app',
+    storageBucket: '',
+    messagingSenderId: '554429806658'
+  }
 };
