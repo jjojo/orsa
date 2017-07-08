735fd4d381e71682ee400525ebd1a9dfb318f5fe
lojdquist <axel.lojdquist@hotmail.com>
2017-07-08 18:24:48 +0200

    jonas var på toa

--- orsa-score/src/app/admin/admin.component.ts
+++ orsa-score/src/app/admin/admin.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+﻿import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-admin',
-  templateUrl: './admin.component.html',
-  styleUrls: ['./admin.component..styl']
+    selector: 'app-admin',
+    templateUrl: './admin.component.html',
+    styleUrls: ['./admin.component..styl']
 })
+
+
+
 export class AdminComponent implements OnInit {
 
-  constructor() { }
+    constructor() {
+    }
 
-  ngOnInit() {
-  }
+    scoreSuggestion = [
+      {id: 0, name: "test", by: "Test2", points: 2, motivation: "Jesper luktar bajs"},
+      {id: 1, name: "test2", by: "Test", points: 0, motivation: "Eller gör han det?"}
+    ];
+
+
+    ngOnInit() {
+    }
 
 }
