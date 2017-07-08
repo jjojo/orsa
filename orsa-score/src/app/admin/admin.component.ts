409b242357d89c6c9f8e37dcc8c51302e1078bd9
lojdquist <axel.lojdquist@hotmail.com>
2017-07-08 18:40:54 +0200

    Jesper går

--- orsa-score/src/app/admin/admin.component.ts
+++ orsa-score/src/app/admin/admin.component.ts
@@ -15,11 +15,19 @@ export class AdminComponent implements OnInit {
 
     scoreSuggestion = [
       {id: 0, name: "test", by: "Test2", points: 2, motivation: "Jesper luktar bajs"},
-      {id: 1, name: "test2", by: "Test", points: 0, motivation: "Eller gör han det?"}
+      {id: 1, name: "test2", by: "Test", points: 0, motivation: "Eller gör han det? as d sad sad sa da sd sad ads as das d ad asd asd asd asd asd as d asd as dsa a sd sad dsa dsa ads ads dsa dsa das dsa ads dsa ads dsa  dsa dsa dsa ads"}
     ];
 
 
     ngOnInit() {
     }
 
+    addpoints(points) {
+        points++;
+        console.log(points);
+    }
+
+
+
 }
+
